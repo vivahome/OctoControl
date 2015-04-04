25e5661bcbbbc4d946e779d19e14ae25568476b0
adeeb <quillford@users.noreply.github.com>
2015-04-04 11:57:20 -0700

    collapsable navbar

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // OctoControl
+
+window.onload = function(){
+  switchView("home");
+};
+
+function switchView(newView){
+  $("#main-body").empty();
+  $("#main-body").load("screens/"+newView+".html");
+}
